6ff60fc3d655e4dec19554c305291cbed5ed745e
anonimzwx <pndiaz1@uc.cl>
2026-05-20 19:32:04 -0500

    library palette effect creator

---
+++ DynamicXtremePaletteCreatorLibrary/PaletteEffect.cs
@@ -0,0 +1,60 @@
+﻿namespace DynamicXtremePaletteCreatorLibrary
+{
+    public enum Channels { RGB = 0, HSL = 1 }
+    public enum EffectType
+    {
+        None = -1,
+        MixRGB = 0,
+        MixHSL = 1,
+        MixRG = 2,
+        MixRB = 3,
+        MixGB = 4,
+        MixHS = 5,
+        MixHL = 6,
+        MixSL = 7,
+        MixR = 8,
+        MixG = 9,
+        MixB = 10,
+        MixH = 11,
+        MixS = 12,
+        MixL = 13,
+        PalTransitionRGB = 14,
+        PalTransitionHSL = 15,
+        PalFunctionRGB = 16,
+        PalFunctionHSL = 17
+    }
+    [Serializable]
+    public class PaletteEffect
+    {
+        public byte Channel1 { get; set; }
+        public byte Channel2 { get; set; }
+        public byte Channel3 { get; set; }
+        public byte Ratio1 { get; set; }
+        public byte Ratio2 { get; set; }
+        public byte Ratio3 { get; set; }
+        public EffectType EffectType { get; set; }
+        public PaletteEffect()
+        {
+
+        }
+        public static EffectType GetEffectType(Channels channels, byte r1, byte r2, byte r3)
+        {
+            int value = (int)channels;
+            if (r1 != 0 && r2 != 0 && r3 != 0)
+                return (EffectType)value;
+            if (r1 != 0 && r2 != 0)
+                return (EffectType)(value + 2);
+            if (r1 != 0 && r3 != 0)
+                return (EffectType)(value + 4);
+            if (r2 != 0 && r3 != 0)
+                return (EffectType)(value + 6);
+            if (r1 != 0)
+                return (EffectType)(value + 8);
+            if (r2 != 0)
+                return (EffectType)(value + 10);
+            if (r3 != 0)
+                return (EffectType)(value + 12);
+            return EffectType.None;
+        }
+    }
+}
---
+++ DynamicXtremePaletteCreatorLibrary/PaletteEffectCollection.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace DynamicXtremePaletteCreatorLibrary
+{
+    [Serializable]
+    public class PaletteEffectCollection
+    {
+        public string? Name { get; set; }
+        public List<PaletteEffect> Effects { get; set; } = [];
+        public PaletteEffect this[int index] => Effects[index];
+        public PaletteEffectCollection()
+        {
+        }
+        public void Add(PaletteEffect effect) => Effects.Add(effect);
+        public void AddAt(PaletteEffect effect, int index) => Effects.Insert(index, effect);
+        public void RemoveAt(int  index) => Effects.RemoveAt(index);
+        public bool Load(string path)
+        {
+            string jsonContent = File.ReadAllText(path);
+            try
+            {
+                var pec = JsonSerializer.Deserialize<PaletteEffectCollection>(jsonContent.Trim());
+                if (pec == null)
+                    return false;
+                Effects = pec.Effects;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public void Save(string path)
+        {
+            string json = JsonSerializer.Serialize(this,new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            });
+            if(File.Exists(path)) 
+                File.Delete(path);
+            File.WriteAllText(path, json);
+        }
+    }
+}
